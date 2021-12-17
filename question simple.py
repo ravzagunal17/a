dc7ca6a7d52cee4959b576b2c6464acd0e748efc
ravzagunal17 <gravza971@gmail.com>
2021-12-17 20:35:53 +0300

    Initial commit

---
+++ question simple.py
@@ -0,0 +1,20 @@
+def GeminiGeneral():
+    gemini_general = " Sembolü: İkizler \n Elementi: Hava \n Niteliği: Değişken \n Yönetici gezegeni: Merkür (iletişim ve haberleşmenin gezegeni) \n Renk: Açık yeşil, sarı" \
+                 "Uğurlu sayılar: 5, 7 \n Uğurlu taşları: Akuamarin, Akik, Topaz, Sitrin, Turmalin" \
+                 "\n Olumlu özellikleri: Etkileyici, özgün, iş bilen, alımlı, akıllı, maceracı" \
+                 "\n Olumsuz özellikleri: Vesveseli, dengesiz, iki yüzlü, yargılayıcı, depresif" \
+                 "\n En sevdiği şeyler: Müzik, kitaplar, dergiler, sohbet etmek, kısa gezintiler" \
+                 "\n Nefret ettiği şeyler: Yalnız olmak, sınırlandırılmak, tekrar ve rutin, dar görüşlü insanlar, geleneksel moda, otorite figürleri, sessizlik"
+    print(gemini_general)
+
+
+
+def CancerGeneral():
+    cancer_general = " Sembolü: Yengeç \n Elementi: Su \n Niteliği: Kardinal(öncü) \n Yönetici gezegeni: Ay (duyguları ve ruhsal durumları yöneten gök cismi) \n Renk: Beyaz" \
+                "Uğurlu sayılar: 2, 3 \n Uğurlu taşları: Akuamarin, Ay taşı, Kuvars kristali, Safir Taşı, Zümrüt" \
+                "\n Olumlu özellikleri: Yardımsever, sabırlı, merhametli, anaç, romantik, yaratıcı" \
+                "\n Olumsuz özellikleri: Dedikoducu, burnu havada, ketum, aşırı duyarlı, fazla rekabetçi" \
+                "\n En sevdiği şeyler: Gurme yemekler, salon sporları, ev partileri, çocuklar, müze ve sanat galeriler, evde yapılan hobiler, denize girmek, sevdiklerine yardım etmek" \
+                "\n Nefret ettiği şeyler: Pejmürde giysiler, uluorta konuşmak, acele ettirilmek, yabancı insanlar, özel hayatını anlatmak"
+    print(cancer_general)
+
